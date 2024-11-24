13e7e3bd8cb474337f14f890a72359cd153040b5
daichi904 <minimi.904.horo@gmail.com>
2024-11-25 01:19:37 +0900

    commit

--- universityClass/programingExercise-2/report1/2-A.c
+++ universityClass/programingExercise-2/report1/2-A.c
@@ -2,41 +2,37 @@
 #include <stdlib.h>
 #include <malloc.h>
 
-// 教科書 p.21~22 に対応
-#define MAX_GRAPH_SIZE 30 // The maximum size which this program can process.
+// The maximum size which this program can process.
+#define MAX_GRAPH_SIZE 30
 
-// 隣接行列用のデータ構造の定義
-typedef int AdjacencyMatrix[MAX_GRAPH_SIZE][MAX_GRAPH_SIZE]; // テキストでは変数定義になっているが,ここでは型として宣言
+typedef int AdjacencyMatrix[MAX_GRAPH_SIZE][MAX_GRAPH_SIZE];
 
-// 隣接リスト用のデータ構造の定義
 typedef int VertexIndex;
 
 typedef struct EdgeCell
-{                            // 有向辺のデータ構造
-    VertexIndex destination; // 有向辺の終点 destination
-    struct EdgeCell *next;   // 始点を同じくする,他の有向辺へのポインタ
+{
+    VertexIndex destination;
+    struct EdgeCell *next;
 } EdgeCell;
 
-typedef EdgeCell *vertices[MAX_GRAPH_SIZE];
-
-// テキストを参考にしたのはここまで
+typedef EdgeCell *Vertices[MAX_GRAPH_SIZE];
 
 typedef struct
-{                  // グラフのデータ構造
-    int vertexNum; // 頂点数
-    int edgeNum;   // 辺の数
-    vertices vtop; // 頂点のデータ構造
+{
+    int vertexNum;
+    int edgeNum;
+    Vertices vtop;
 } Graph;
 
-// 隣接行列をファイル datafile から読み込む
+// Import AdjacencyMatrix file from datafile.
 int readAdjacencyMatrix(char *datafile, AdjacencyMatrix mat)
 {
-    FILE *fp; // 入力データのファイルポインタ
+    FILE *fp;
     int vertexNum;
     VertexIndex src, dest;
 
-    fp = fopen(datafile, "r");      // ファイルを開く
-    fscanf(fp, "%d\n", &vertexNum); // 頂点数 の読み込み
+    fp = fopen(datafile, "r");
+    fscanf(fp, "%d\n", &vertexNum);
     if (vertexNum > MAX_GRAPH_SIZE)
     {
         fprintf(stderr, "##### This programm cannot process the size of graph which over %d.\n", MAX_GRAPH_SIZE);
@@ -46,15 +42,15 @@ int readAdjacencyMatrix(char *datafile, AdjacencyMatrix mat)
     {
         for (dest = 0; dest < vertexNum; dest++)
         {
-            fscanf(fp, "%d\n", &mat[src][dest]); // 隣接行列の要素を1つずつ読み込む
+            fscanf(fp, "%d\n", &mat[src][dest]);
         }
     }
-    fclose(fp); // ファイルを閉じる
+    fclose(fp);
 
     return vertexNum;
 }
 
-// 辺の追加 辺は頂点番号 src から dest へ向う
+// Add edge.
 void addEdge(Graph *g, VertexIndex src, VertexIndex dest)
 {
     EdgeCell *edge = (EdgeCell *)malloc(sizeof(EdgeCell));
@@ -63,12 +59,9 @@ void addEdge(Graph *g, VertexIndex src, VertexIndex dest)
     g->vtop[src] = edge;
 }
 
-// 隣接行列からグラフを作成 (テキスト指定のデータ構造)
+// Create graph adjacency matrix.
 void translateIntoGraph(AdjacencyMatrix mat, Graph *g)
 {
-    // この関数を完成させよ
-    // 隣接行列の要素が 1 なら有向辺があるということ → 有向辺を追加 (関数 addEdge を使う)
-
     VertexIndex i, j;
     for (i = 0; i < g->vertexNum; i++)
     {
@@ -86,6 +79,7 @@ void translateIntoGraph(AdjacencyMatrix mat, Graph *g)
     }
 }
 
+// Print graph.
 void printGraph(Graph *g)
 {
     VertexIndex v;
@@ -101,7 +95,8 @@ void printGraph(Graph *g)
     }
     printf("}\n");
 }
-// グラフ g の使用しているメモリを解放
+
+// Free the memory which graph is using.
 void freeGraph(Graph *g)
 {
     VertexIndex v;
@@ -116,6 +111,7 @@ void freeGraph(Graph *g)
     }
 }
 
+// Perform DFS 
 void dfs(int start, int *visited, Graph *g, AdjacencyMatrix *mat)
 {
     visited[start] = 1;
@@ -128,7 +124,8 @@ void dfs(int start, int *visited, Graph *g, AdjacencyMatrix *mat)
     }
 }
 
-void rdfs(int start, int *reversedVisited, Graph *g, AdjacencyMatrix *mat)
+// Perform DFS
+void reversedDfs(int start, int *reversedVisited, Graph *g, AdjacencyMatrix *mat)
 {
     reversedVisited[start] = 1;
     for (int i = 0; i < g->vertexNum; i++)
@@ -150,34 +147,34 @@ int isStronglyConnected(int *visited, int *reversedVisited, Graph *g, AdjacencyM
     {
         if (!visited[i])
         {
-            return 0; // 全ての頂点に到達していない
+            return 0; // Reached to none of vertex.
         }
     }
 
-    rdfs(start, reversedVisited, g, a);
+    reversedDfs(start, reversedVisited, g, a);
 
     for (int i = 0; i < g->vertexNum; i++)
     {
         if (!reversedVisited[i])
         {
-            return 0; // 全ての頂点に到達していない
+            return 0; // Reached to none of vertex.
         }
     }
 
-    return 1; // 全ての頂点に到達している
+    return 1; // Reached to all of vertex.
 }
 
 int main(int argc, char *argv[])
 {
-    char *datafile;    // 入力データのファイル名
-    AdjacencyMatrix a; // テキストでは大文字 A で定義
+    char *datafile;
+    AdjacencyMatrix a;
     Graph g;
     if (argc <= 1)
     {
-        fprintf(stderr, "#####ファイル名を指定してください\n");
+        fprintf(stderr, "##### Please specify a file name.\n");
         return 1;
     }
-    datafile = argv[1]; // ファイル名の取得
+    datafile = argv[1];
     g.vertexNum = readAdjacencyMatrix(datafile, a);
     translateIntoGraph(a, &g);
 
@@ -193,6 +190,7 @@ int main(int argc, char *argv[])
         printf("n\n");
     }
 
-    freeGraph(&g); // グラフのデータ格納場所を解放
+    printGraph(&g);
+    freeGraph(&g);
     return 0;
 }
